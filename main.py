9919a139b1da794d405b2d7693b443b693b4f0a8
elisabethkollrack <ekollrac@uvm.edu>
2024-09-30 16:56:16 -0400

    created data set with chem lectures

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+"""
+Elisabeth Kollrack
+Scheduling Conflict Program for TC
+"""
+
+import pandas as pd
+
+df = pd.read_csv('/Users/elisabethkollrack/Tutoring/enrollment_f24.csv')
+
+
+filtered_selected_df = df.loc[(df[' Subj'] == 'CHEM') & (df['#'] == '1400') & (df['Lec Lab'] == 'LEC'), [' Subj', '#', 'Title', 'Sec', 'Start Time', 'End Time']]
+
+print(filtered_selected_df)
