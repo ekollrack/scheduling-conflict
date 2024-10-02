2992fe8e6533ce25625ea9d9ed1c057f0ec60fda
elisabethkollrack <ekollrac@uvm.edu>
2024-10-02 11:33:26 -0400

    added a way to check for conflicts within tuples

--- main.py
+++ main.py
@@ -11,4 +11,27 @@ df = pd.read_csv('/Users/elisabethkollrack/Tutoring/enrollment_f24.csv')
 
 filtered_selected_df = df.loc[(df[' Subj'] == 'CHEM') & (df['#'] == '1400') & (df['Lec Lab'] == 'LEC'), [' Subj', '#', 'Title', 'Sec', 'Start Time', 'End Time']]
 
-print(filtered_selected_df)
+
+def check_conflict(tutor_schedule, lecture_schedule):
+    conflicts = []
+    for tutor_slot in tutor_schedule:
+        tutor_start, tutor_end = tutor_slot
+        for lecture_slot in lecture_schedule:
+            lecture_start, lecture_end = lecture_slot
+            if tutor_start < lecture_end and lecture_start < tutor_end:
+                conflicts.append((tutor_slot, lecture_slot))
+    return conflicts
+
+# Test schedules
+tutor_schedule = [(8, 9), (12, 13)]
+lecture_schedule = [(8, 10), (12,14),(15,16)]
+
+
+has_conflict = check_conflict(tutor_schedule, lecture_schedule)
+if has_conflict:
+    for tutor_slot, lecture_slot in has_conflict:
+        print(f"Scheduling conflict detected between tutor slot {tutor_slot} and lecture slot {lecture_slot}")
+else:
+    print("No scheduling conflict.")
+
+
