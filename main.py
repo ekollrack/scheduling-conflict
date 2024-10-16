0cd638bec8df90f8e1ea35c9fd946eefc008f943
elisabethkollrack <ekollrac@uvm.edu>
2024-10-16 14:13:50 -0400

    need to do input validation and days of the week

--- main.py
+++ main.py
@@ -5,24 +5,37 @@ Takes input about which class they tutor for and which times they are scheduled 
 and checks for scheduling conflicts
 """
 
+# TODO
+# Input validation
+# Days of the week
+
 
 import pandas as pd
 
 
 """
 Inputs: Tutor and lecture schedules
-Outputs: a list of conflicting times
+Outputs: a list of conflicting times or that there are no conflicts
 Effects: Checks for overlap between the tutor's schedule and lecture times
 """
-def check_conflict(tutor_schedule, lecture_schedule):
-    conflicts = []
-    for tutor_slot in tutor_schedule:
+def check_conflict(tutor, lecture):
+    conflicts = set()
+    for tutor_slot in tutor:
         tutor_start, tutor_end = tutor_slot
-        for lecture_slot in lecture_schedule:
+        for lecture_slot in lecture:
             lecture_start, lecture_end = lecture_slot
             if tutor_start < lecture_end and lecture_start < tutor_end:
-                conflicts.append((tutor_slot, lecture_slot))
-    return conflicts
+                conflicts.add((tutor_slot, lecture_slot))
+
+    # Prints conflicts if there are any
+    if conflicts:
+        for (tutor_start, tutor_end), (lecture_start, lecture_end) in conflicts:
+            print(f"Conflict: Tutor slot {reverse_time_conversion(tutor_start)}-"
+                  f"{reverse_time_conversion(tutor_end)} and Lecture slot "
+                  f"{reverse_time_conversion(lecture_start)}-"
+                  f"{reverse_time_conversion(lecture_end)}")
+    else:
+        print("No scheduling conflict.")
 
 
 """
@@ -41,11 +54,11 @@ Inputs: List of times the user inputted
 Outputs: The list of times in decimal format
 Effects: Converts inputted list of times to a decimal format
 """
-def convert_tutoring_times(user_input):
+def convert_tutoring_times(times):
     tutoring_times = []
     converted_tutoring_times = []
     # This for loop formats times as [(time 1: time 2),(time 3: time 4)]
-    for t in user_input.split(','):
+    for t in times.split(','):
         start, end = t.split('-')
         tutoring_times.append((start, end))
     # This for loop formats times as [(time 1, time 2),(time 3, time 4)]
@@ -54,20 +67,6 @@ def convert_tutoring_times(user_input):
         converted_end = time_conversion(end)
         converted_tutoring_times.append((converted_start, converted_end))
     return converted_tutoring_times
-
-
-"""
-Inputs: List of lecture times for the specified class
-Outputs: The list of times in decimal format
-Effects: Converts inputted list of times to a decimal format
-"""
-def convert_lecture_times(lec_df):
-    lecture_schedule = []
-    for start, end in lec_df[['Start Time', 'End Time']].values:
-        converted_start = time_conversion(start)
-        converted_end = time_conversion(end)
-        lecture_schedule.append((converted_start, converted_end))
-    return lecture_schedule
 
 
 """
@@ -87,9 +86,9 @@ def reverse_time_conversion(decimal_time):
 
 
 """
-Inputs: Class description
-Outputs: Data frame of class information
-Effects: Creates a data frame from enrollment data
+Inputs: Class name and number
+Outputs: Lecture schedule for that class number
+Effects: Creates a list of tuples of lecture times
 """
 def lectures_data_frame(name,number):
     df = pd.read_csv('enrollment_f24.csv')
@@ -98,36 +97,26 @@ def lectures_data_frame(name,number):
            (df['Lec Lab'] == 'LEC') &
            (df['Start Time'] != 'Nan') &
            (df['#'] == number)]
-    return df
 
-# TODO
-# Remove duplicate tuples
-# Function for check conflict
-# Input validation
-# Days of the week
+    schedule = []
+    for start, end in df[['Start Time', 'End Time']].values:
+        converted_start = time_conversion(start)
+        converted_end = time_conversion(end)
+        schedule.append((converted_start, converted_end))
+    return schedule
+
+
 
 if __name__ == "__main__":
 
     user_input = input("Enter your tutoring times in the format hh:mm - hh:mm, hh:mm - hh:mm:\nex: 13:00-14:00, 09:45-11:30\n")
     tutoring_schedule = convert_tutoring_times(user_input)
+
     class_choice = input("Which class would you like to schedule conflicts for? Enter course abbreviation e.g. 'CHEM'\n").upper()
     class_number = input("What is the course number? e.g 1400\n")
 
-
-    # Finds the class in the enrollment dataset
-    lecture_data = lectures_data_frame(class_choice,class_number)
     # Finds the lecture times and adds to lecture schedule
-    lecture_schedule = convert_lecture_times(lecture_data)
+    lecture_schedule = lectures_data_frame(class_choice,class_number)
 
-    # Check for conflicts and print the results
-    has_conflict = check_conflict(tutoring_schedule, lecture_schedule)
-    if has_conflict:
-        for (tutor_start, tutor_end), (lecture_start, lecture_end) in has_conflict:
-            print(f"Conflict: Tutor slot {reverse_time_conversion(tutor_start)}-"
-                  f"{reverse_time_conversion(tutor_end)} and Lecture slot "
-                  f"{reverse_time_conversion(lecture_start)}-"
-                  f"{reverse_time_conversion(lecture_end)}")
-    else:
-        print("No scheduling conflict.")
-
-    print(lecture_schedule)
+    # Check for conflicts
+    check_conflict(tutoring_schedule, lecture_schedule)
